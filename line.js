6e6d4dc478fe21b3550268c8d4ba36888dd3dd0b
ankokovin <lena13u@yandex.ru>
2019-09-30 23:09:46 +0500

    first task

---
+++ line.js
@@ -0,0 +1,71 @@
+prad = 10
+class Line{
+  constructor(p1, p2){
+    this.p1 = p1;
+    this.p2 = p2;
+    this.isSelected = false;
+  }
+  
+  get_points(){
+    return [this.p1, this.p2];
+  }
+  
+  
+  close_point(mx, my){
+    var res = null;
+    var d = dist(mx, my, this.p1.x, this.p1.y);
+    if (d < prad)
+        res = [1, this.p1];
+    var d2 = dist(mx, my, this.p2.x, this.p2.y);
+    if (d2 < prad && d2<d)
+      res = [2, this.p2];
+
+    if (res == null){
+      var a = p5.Vector.sub(createVector(mx,my),this.p1);
+      var b = p5.Vector.sub(this.p2, this.p1);
+      var t = p5.Vector.dot(a,b) / b.mag();
+      var p = b;
+      p.normalize();
+      p.mult(t);
+      p.add(this.p1);
+      d = dist(p.x, p.y, mx, my)
+      if (d < prad)
+        res = [0, p];
+    }
+    
+    return res;
+  }
+  
+  update(p){
+     if(p == 1)
+     {
+       this.p1.x = mouseX;
+       this.p1.y = mouseY;
+     }
+    if (p == 2)
+    {
+      this.p2.x = mouseX;
+      this.p2.y = mouseY;
+    }
+  }
+
+
+  updateParallel(v1,v2){
+    this.p1.x = mouseX+v1.x;
+    this.p1.y = mouseY+v1.y;
+    this.p2.x = mouseX+v2.x;
+    this.p2.y = mouseY+v2.y;
+  }
+  
+  
+  show(){
+    if (this.isSelected)
+    stroke('red');
+    else
+      stroke('black');
+    line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+    circle(this.p1.x, this.p1.y, prad);
+    circle(this.p2.x, this.p2.y, prad);
+  }
+  
+}
---
+++ sketch.js
@@ -0,0 +1,97 @@
+const width = 600;
+const height = 400;
+lines = []
+
+selected_line = -1;
+pressed_point = -1;
+var v1 = null;
+var v2 = null;
+
+function setup() {
+  print(p5.Vector.dot(createVector(1,2,3),createVector(2,3,4)));
+  cv = createCanvas(width, height);
+  cv.mousePressed(onMousePressed);
+  cv.mouseReleased(onMouseReleased);
+  noLoop();
+  addbt = createButton("Добавить");
+  addbt.position(width+35,10);
+  addbt.mousePressed(generateLine);
+  rembt = createButton("Удалить");
+  rembt.position(width+35, 40);
+  rembt.attribute('disabled');
+  rembt.mousePressed(onDelete);
+}
+
+function generateLine(){
+  p1 = createVector(random(width), random(height));
+  p2 = createVector(random(width), random(height));
+  lines.push(new Line(p1,p2));
+  redraw();
+}
+
+function onMousePressed(){
+  let mx = mouseX, my = mouseY;
+  if (selected_line != -1)
+    lines[selected_line].isSelected = false;
+  
+  selected_line = -1;
+  pressed_point = -1;
+  
+  var curmax = null;
+  var curd = 0;
+  var lineid = -1;
+  for (let i=0; i<lines.length;++i){
+     let res = lines[i].close_point(mx, my);
+     if (res != null){
+      var d = dist(mx, my, res[1].x, res[1].y);
+      if (curmax == null || d<curd){
+        curmax = res;
+        curd = d;
+        lineid = i;
+      }
+    }
+  }
+  if (curmax != null){
+    selected_line = lineid;
+    pressed_point = curmax[0];
+    lines[selected_line].isSelected = true;
+    if (pressed_point == 0)
+    {
+      let points = lines[selected_line].get_points();
+      v1 = createVector(points[0].x - mx, points[0].y - my);
+      v2 = createVector(points[1].x - mx, points[1].y - my);
+    }
+  }
+  redraw();
+}
+
+function onDelete(){
+  if (selected_line!=-1){
+    lines.splice(selected_line,1);
+    redraw();
+  }
+}
+
+function mouseDragged(){
+  if (selected_line != -1 && pressed_point != -1){
+    if (pressed_point == 0)
+      lines[selected_line].updateParallel(v1,v2);
+    else
+      lines[selected_line].update(pressed_point);
+
+    redraw();
+  }
+}
+
+function onMouseReleased(){
+  redraw();
+  pressed_point = -1;
+}
+
+function draw() {
+  background(200);
+  for (let i=0; i<lines.length;++i)
+  {
+      lines[i].show();
+  }
+}
